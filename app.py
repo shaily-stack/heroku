ff16644e83a1c1b60e16528c938ebbe634fc97db
shaily-stack <62254375+shaily-stack@users.noreply.github.com>
2021-06-06 23:02:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 16 14:45:13 2020
+
+@author: Shaily
+"""
+
+# Importing essential libraries
+
+from flask import Flask,render_template, url_for,request
+import pandas as pd 
+import os
+import pickle
+
+import numpy as np
+
+# Load the Random Forest CLassifier model
+model = 'xgboost_random_model.pkl'
+regressor = pickle.load(open(model, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        avg_temp = float(request.form['Average_temp'])
+        max_temp = float(request.form['Max_temp'])
+        min_temp = float(request.form['Min_temp'])
+        at_pres = float(request.form['Atmospheric_pressure'])
+        avg_hum = float(request.form['Average_humidity'])
+        avg_rain = float(request.form['Total_rainfall'])
+        avg_vis = float(request.form['Average_visibility'])
+        avg_speed = float(request.form['Average_windspeed'])
+        max_sustained = float(request.form['Max sustained wind speed'])
+        
+        data = np.array([[avg_temp,max_temp, min_temp, at_pres, avg_hum, avg_rain, avg_vis, avg_speed, max_sustained]])
+        my_prediction = regressor.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+
+if __name__ == '__main__':
+	app.run(port=5000, debug=True)
